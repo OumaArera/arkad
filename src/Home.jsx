f811b6a2948ed0c091f64885ac757e17ff9d4443
OumaArera <johnouma999@gmail.com>
2024-11-01 15:58:08 +0300

    feat: complete

--- src/Home.jsx
+++ src/Home.jsx
@@ -145,7 +145,7 @@ const Home = () => {
 
         {/* Partners Section */}
         <div className="mt-12 flex flex-col items-center space-y-4">
-          <div className='mt-4 font-bold text-white text-lg'>Our Partners</div>
+          <div className='mt-4 font-bold text-white text-2xl'>Our Partners</div>
           <div className="flex items-center space-x-4">
             {/* Left Arrow */}
             <button
